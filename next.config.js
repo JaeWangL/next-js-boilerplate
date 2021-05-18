ee7e1289b6d50d3e25e2b8c3fd7b0413e06638c2
JaeWangL <jnso5072@outlook.kr>
2021-05-18 20:06:59 +0900

    [Feature] Add  and  for SEO

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withAntdLess = require('next-plugin-antd-less');
+const sitemap = require('nextjs-sitemap-generator');
 const { i18n } = require('./next-i18next.config');
+
+sitemap({
+  pagesDirectory: `${__dirname}/src/pages`,
+  targetDirectory: `public/`,
+});
 
 module.exports = withAntdLess({
   lessVarsFilePathAppendToEndOfContent: false,
